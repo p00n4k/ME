5af382b815c3f9428c3f2cfc0d672c92b2cffeee
p00n4k <105700844+p00n4k@users.noreply.github.com>
2025-02-03 13:53:17 +0700

    Update LinkBoard.tsx

--- components/LinkBoard/LinkBoard.tsx
+++ components/LinkBoard/LinkBoard.tsx
@@ -2,18 +2,25 @@ import styles from './LinkBoard.module.css';
 const LinkBoard = () => {
     return (
         <div>      <a href="https://www.canva.com/design/DAGd-rERVtc/Hl7R-_F_qGLZ6KeDHYyBYQ/edit" target="_blank" rel="noopener noreferrer" className={styles.geekbutton} >
-            Vision Board
+            📌 Vision Board
         </a>
             <a href="https://www.blogger.com/blog/posts/7835376191365107012" target="_blank" rel="noopener noreferrer" className={styles.geekbutton}>
-                Book Review
+                📚 Book Review
             </a>
             <a href="https://play.google.com/store/apps/details?id=com.gistda.gflood_warning_beta&hl=th" target="_blank" rel="noopener noreferrer" className={styles.geekbutton}>
-                Work
+                💻 Work
             </a>
             <a href="https://www.canva.com/design/DAGeAmRqYfc/U4vnm3iVcUsgSuCC2sASCw/edit" target="_blank" rel="noopener noreferrer" className={styles.geekbutton}>
-                Resume
+                📝 Resume
             </a>
             <br />
+            <a href="/" className={styles.geekbutton}> 🕹️ Hobby
+            </a>
+            <a href="/" className={styles.geekbutton}> 📊 Project
+            </a>
+
+
+
 
 
 
